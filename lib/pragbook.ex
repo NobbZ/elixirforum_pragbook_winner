ffc83673449514bb0d7b7c37a6dc36375cf340d9
Norbert Melzer <timmelzer@gmail.com>
2016-04-08 10:38:12 +0200

    get specific month/current month

--- lib/pragbook.ex
+++ lib/pragbook.ex
@@ -1,7 +1,13 @@
 defmodule Pragbook do
 
   def main([]) do
-    site = HTTPotion.get("http://pragprog.elixirforum.com/april").body
+    get_month
+    |> (fn n -> [n] end).()
+    |> main
+  end
+  
+  def main([month]) do
+    site = HTTPotion.get("http://pragprog.elixirforum.com/#{month}").body
 
     entries = site
     |> Floki.find(".thismonthsentriestable")
@@ -40,5 +46,24 @@ defmodule Pragbook do
 
   defp inject_difference({this, name}, number), do: {abs(number - this), this, name}
 
-  defp print_result({diff, ticket, name}, target), do: IO.puts "#{name} had the ticket #{ticket} which is only #{diff} away from #{target}" 
+  defp print_result({diff, ticket, name}, target), do: IO.puts "#{name} had the ticket #{ticket} which is only #{diff} away from #{target}"
+
+  defp get_month do
+    :calendar.universal_time
+    |> (fn ({{_, m, _}, _}) -> m end).()
+    |> make_month_name_from_int
+  end
+
+  defp make_month_name_from_int(1), do: "january"
+  defp make_month_name_from_int(2), do: "february"
+  defp make_month_name_from_int(3), do: "march"
+  defp make_month_name_from_int(4), do: "april"
+  defp make_month_name_from_int(5), do: "may"
+  defp make_month_name_from_int(6), do: "june"
+  defp make_month_name_from_int(7), do: "july"
+  defp make_month_name_from_int(8), do: "august"
+  defp make_month_name_from_int(9), do: "septembre"
+  defp make_month_name_from_int(10), do: "octobre"
+  defp make_month_name_from_int(11), do: "novembre"
+  defp make_month_name_from_int(12), do: "decembre"
 end
